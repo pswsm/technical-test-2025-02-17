5002fd72125da796066d90e555a0fa7c9c5e5503
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-18 18:45:34 +0100

    feat: device component

---
+++ src/lib/components/Device.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Device } from '$lib/device/domain/Device';
+
+	export let device: Device;
+</script>
+
+<div id={device.getName().valueOf()}>
+	<img src={device.getImage().valueOf()} alt="device" />
+	<p>{device.getDescription().valueOf()}</p>
+</div>
