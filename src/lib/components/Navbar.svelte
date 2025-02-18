a88b50f6c407b2b18cedd976f31c0c3101dcfc38
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-18 14:57:18 +0100

    feat: add navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,11 @@
+<div class="flex w-full justify-between bg-zinc-200 px-6 py-4 md:px-4 dark:bg-zinc-900">
+	<div>
+		<a href="/">
+			<img src="/logo-alexphone-main-header.svg" alt="AlexPhone Logo" aria-label="AlexPhone Logo" />
+			<!-- logo -->
+		</a>
+	</div>
+	<div class="flex">
+		<!-- carreto -->
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<Navbar />
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
