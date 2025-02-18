bd573e5da2f3185d11ed23f95241a3595435fa76
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-18 18:02:51 +0100

    refactor: move navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,12 @@
-<div class="flex w-full justify-between bg-zinc-200 px-6 py-4 md:px-4 dark:bg-zinc-900">
+<div class="flex w-full content-center justify-between bg-zinc-900 px-12 py-4 text-white md:px-6">
 	<div>
 		<a href="/">
 			<img src="/logo-alexphone-main-header.svg" alt="AlexPhone Logo" aria-label="AlexPhone Logo" />
 			<!-- logo -->
 		</a>
 	</div>
-	<div class="flex">
+	<div>
 		<!-- carreto -->
+		<div><a href="/#">Carreto</a></div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<script>
-	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<Navbar />
 <h1>Welcome to SvelteKit</h1>
