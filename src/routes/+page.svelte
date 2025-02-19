905b5b93cdf3a24f8d0220d819788a03104b03ca
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-19 19:10:15 +0100

    feat: filters!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,65 @@
 	import Grid from '$lib/components/Grid.svelte';
 	import type { PageProps } from './$types';
 
+	let form: HTMLFormElement | undefined = $state();
 	let { data }: PageProps = $props();
+	let fromSlider = $state(data.minPrice);
+	let toSlider = $state(data.maxPrice);
+	let selectedGrade = $state(data.params.get('grade') || 'none');
 </script>
 
 <svelte:head>
 	<title>AlexPhone</title>
 </svelte:head>
 <section id="main" class="mx-auto w-2/3">
-	{#await data.devices}
+	{#await data.visibleDevices}
 		<p>loading skus...</p>
 	{:then devices}
+		<form bind:this={form}>
+			<div class="flex">
+				<div>
+					<label class="pr-2"
+						>From: {fromSlider}
+						<input
+							type="range"
+							min={data.minPrice}
+							max={data.maxPrice}
+							step="1"
+							bind:value={fromSlider}
+							name="from"
+							onchange={() => form?.requestSubmit()}
+						/>
+					</label>
+				</div>
+				<div>
+					<label class="pr-2"
+						>To: {toSlider}
+						<input
+							type="range"
+							min={data.minPrice}
+							max={data.maxPrice}
+							step="1"
+							bind:value={toSlider}
+							name="to"
+							onchange={() => form?.requestSubmit()}
+						/></label
+					>
+				</div>
+				<div>
+					<select
+						name="grade"
+						id="grade"
+						bind:value={selectedGrade}
+						onchange={() => form?.requestSubmit()}
+					>
+						<option value="none">None</option>
+						{#each data.grades as grade}
+							<option value={grade.valueOf()}>{grade.valueOf()}</option>
+						{/each}
+					</select>
+				</div>
+			</div>
+		</form>
 		<Grid>
 			{#each devices as device}
 				<Device {device} />
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,15 +1,29 @@
 import type { Device } from '$lib/device/domain/Device';
+import { Grade } from '$lib/device/domain/value-objects/Grade';
 import { DomainMapper } from '$lib/device/infra/DomainMapper';
 import type { ApiSku } from '$lib/types/ApiSku';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ fetch, url }) => {
 	const skus: ApiSku[] = await fetch('https://test.alexphone.com/api/v1/skus').then((data) =>
 		data.json()
 	);
 
+	const params = url.searchParams;
 	const devices: Device[] = skus.map((sku) => DomainMapper.toDomain(sku));
+	const prices: number[] = devices.map((dev) => dev.getPrice().valueOf());
+	const grades = new Set(devices.map((dev) => dev.getGrade().valueOf()));
+	const visibleDevices: Device[] = devices
+		.filter(
+			(dev) => params.get('grade') === 'none' || params.get('grade') === dev.getGrade().valueOf()
+		)
+		.filter((dev) => Number(params.get('from')) <= dev.getPrice().valueOf())
+		.filter((dev) => Number(params.get('to')) >= dev.getPrice().valueOf());
 	return {
-		devices
+		params,
+		maxPrice: Math.max(...prices),
+		minPrice: Math.min(...prices),
+		grades: Array.from(grades).map((grade) => new Grade(grade)),
+		visibleDevices
 	};
 };
