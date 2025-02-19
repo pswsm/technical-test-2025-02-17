c76dbcfc891329d34153037cb622ff85dda85df5
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-20 00:57:32 +0100

    feat: add fontawesome

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,12 @@
-<div class="flex w-full content-center justify-between bg-zinc-900 px-12 py-4 text-white md:px-6">
+<div
+	class="mb-5 flex w-full content-center justify-between bg-zinc-900 px-12 py-4 text-white md:px-6"
+>
 	<div>
 		<a href="/">
 			<img src="/logo-alexphone-main-header.svg" alt="AlexPhone Logo" aria-label="AlexPhone Logo" />
-			<!-- logo -->
 		</a>
 	</div>
 	<div>
-		<!-- carreto -->
-		<div><a href="/#">Carreto</a></div>
+		<div><a href="/carreto" aria-label="Cart"><i class="fa-solid fa-cart-shopping"></i></a></div>
 	</div>
 </div>
