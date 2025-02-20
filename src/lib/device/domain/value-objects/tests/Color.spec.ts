e878013fb82afca3153ed00056e7946217967109
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-20 23:07:26 +0100

    test: update test

--- src/lib/device/domain/value-objects/tests/Color.spec.ts
+++ src/lib/device/domain/value-objects/tests/Color.spec.ts
@@ -1,12 +1,14 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { Color } from '../Color';
 
-test('constructor', () => {
-	const color = new Color('pink');
-	expect(color).toMatchObject({ value: 'pink' });
-});
+describe('Color', () => {
+	test('constructor', () => {
+		const color = new Color('pink');
+		expect(color).toMatchObject({ value: 'pink' });
+	});
 
-test('valueOf', () => {
-	const color = new Color('pink');
-	expect(color.valueOf()).toMatch('pink');
+	test('valueOf', () => {
+		const color = new Color('pink');
+		expect(color.valueOf()).toMatch('pink');
+	});
 });
