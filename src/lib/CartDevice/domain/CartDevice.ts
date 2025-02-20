b42721d2fe6d72966fae717a783c8e53928a95bc
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-20 14:53:21 +0100

    feat: cart device class

---
+++ src/lib/CartDevice/domain/CartDevice.ts
@@ -0,0 +1,40 @@
+import { Device } from '$lib/device/domain/Device';
+import type { Description } from '$lib/device/domain/value-objects/Description';
+import type { DeviceName } from '$lib/device/domain/value-objects/DeviceName';
+import type { Price } from '$lib/device/domain/value-objects/Price';
+import type { PrimitiveOf } from '$lib/shared/PrimitiveOf';
+import { UniqueId } from '$lib/shared/UniqueId';
+
+export class CartDevice {
+	public static fromPrimitives(primitives: PrimitiveOf<CartDevice>) {
+		return new CartDevice(new UniqueId(primitives.id), Device.fromPrimitives(primitives.device));
+	}
+
+	constructor(
+		private readonly id: UniqueId,
+		private readonly device: Device
+	) {}
+
+	public getId(): UniqueId {
+		return this.id;
+	}
+
+	public getDeviceName(): DeviceName {
+		return this.device.getName();
+	}
+
+	public getDevicePrice(): Price {
+		return this.device.getPrice();
+	}
+
+	public getDeviceDescription(): Description {
+		return this.device.getDescription();
+	}
+
+	public toPrimitives() {
+		return {
+			id: this.id.valueOf(),
+			device: this.device.toPrimitives()
+		};
+	}
+}
