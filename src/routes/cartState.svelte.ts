24cd0314245b8e50b6c27db488a7165bfd373ca6
pswsm <53122461+pswsm@users.noreply.github.com>
2025-02-20 14:53:53 +0100

    refactor: better state management through singleton

--- src/routes/cartState.svelte.ts
+++ src/routes/cartState.svelte.ts
@@ -1,3 +1,55 @@
-import type { Device } from '$lib/device/domain/Device';
+import { browser } from '$app/environment';
+import { CartDevice } from '$lib/CartDevice/domain/CartDevice';
+import { Device } from '$lib/device/domain/Device';
+import type { PrimitiveOf } from '$lib/shared/PrimitiveOf';
+import type { UniqueId } from '$lib/shared/UniqueId';
 
-export const cartState: Device[] = $state([]);
+export const CART_LOCALSTORAGE_KEY = 'cart';
+
+export class CartState {
+	public state: CartDevice[] = $state([]);
+
+	constructor() {
+		if (!browser) {
+			return this;
+		}
+		const localStorageCart = localStorage.getItem(CART_LOCALSTORAGE_KEY);
+		if (localStorageCart) {
+			this.state = JSON.parse(atob(localStorageCart)).map((primitives: PrimitiveOf<CartDevice>) =>
+				CartDevice.fromPrimitives(primitives)
+			);
+		}
+	}
+
+	public overwrite(newState: CartDevice[]): void {
+		this.state = newState;
+		if (browser) {
+			localStorage.setItem(
+				CART_LOCALSTORAGE_KEY,
+				btoa(JSON.stringify(this.state.map((d) => d.toPrimitives())))
+			);
+		}
+	}
+
+	public push(cartId: UniqueId, device: Device): void {
+		this.state.push(new CartDevice(cartId, device));
+		if (browser) {
+			localStorage.setItem(
+				CART_LOCALSTORAGE_KEY,
+				btoa(JSON.stringify(this.state.map((device) => device.toPrimitives())))
+			);
+		}
+	}
+
+	public remove(device: CartDevice): void {
+		this.state = this.state.filter((dev) => !dev.getId().equals(device.getId()));
+		if (browser) {
+			localStorage.setItem(
+				CART_LOCALSTORAGE_KEY,
+				btoa(JSON.stringify(this.state.map((d) => d.toPrimitives())))
+			);
+		}
+	}
+}
+
+export const GlobalCartState = new CartState();
